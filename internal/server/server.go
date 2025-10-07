e590a9239b3bbedca69e9020b36efdb10d149727
Niklas Malkusch <niklasmalkusch@googlemail.com>
2025-10-07 22:14:28 +0200

    Fix AllowedHeaders in CORS configuration to ensure case insensitivity for content-type

--- internal/server/server.go
+++ internal/server/server.go
@@ -36,7 +36,7 @@ func (s *Server) Run() error {
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://wgplanner.onrender.com"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedHeaders:   []string{"Content-Type", "content-type", "Authorization"},
 		AllowCredentials: true,
 	})
 	fuego.Use(srv, c.Handler)
